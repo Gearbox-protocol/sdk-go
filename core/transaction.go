56b80b3a561aa8e2eb598c4b30279ccf0938c9dd
harsh jain <harshjniitr@gmail.com>
2022-05-30 23:06:45 +0530

    feature: add getWallet from private key

--- core/transaction.go
+++ core/transaction.go
@@ -1,7 +1,12 @@
 package core
 
 import (
+	"crypto/ecdsa"
 	"math/big"
+
+	"github.com/Gearbox-protocol/sdk-go/log"
+	"github.com/ethereum/go-ethereum/accounts/keystore"
+	"github.com/ethereum/go-ethereum/crypto"
 )
 
 type (
@@ -19,3 +24,19 @@ type (
 		Timestamp        int64    `json:"timestamp"`
 	}
 )
+
+func GetWallet(pk string) keystore.Key {
+	privateKey, err := crypto.HexToECDSA(pk)
+	log.CheckFatal(err)
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("error casting public key to ECDSA")
+	}
+
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	return keystore.Key{
+		Address:    fromAddress,
+		PrivateKey: privateKey,
+	}
+}
