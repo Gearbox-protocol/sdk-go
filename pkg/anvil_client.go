37b48a5799d764014203d68690224d57920e79f4
harsh-98 <harshjniitr@gmail.com>
2023-08-10 01:54:32 +0400

    feat: support anvil 2023-08-09 version

--- pkg/anvil_client.go
+++ pkg/anvil_client.go
@@ -35,19 +35,25 @@ func (anvil *AnvilClient) StopImpersonateAccount(account string) error {
 	return err
 }
 
-func (anvil *AnvilClient) SendAsImpersonator(impAccount common.Address, tx *types.Transaction) common.Hash {
-	defer func() {
-		log.CheckFatal(anvil.StopImpersonateAccount(impAccount.Hex()))
-	}()
+func (anvil *AnvilClient) SendAsImpersonator(impAccount common.Address, tx *types.Transaction, nonce ...int64) common.Hash {
+	// defer func() {
+	// 	log.CheckFatal(anvil.StopImpersonateAccount(impAccount.Hex()))
+	// }()
 	err := anvil.ImpersonateAccount(impAccount.Hex())
 	log.CheckFatal(err)
-	return anvil.SendTransaction(impAccount, tx)
+	if len(nonce) > 0 {
+		return anvil.SendTransaction(impAccount, tx, nonce[0])
+	} else {
+		return anvil.SendTransaction(impAccount, tx)
+	}
 }
 
 type anvilTransaction struct {
 	From            string `json:"from"`
 	To              string `json:"to"`
-	GasPrice        string `json:"gasPrice"`
+	GasPrice        string `json:"gasPrice,omitempty"`
+	MaxPriority     string `json:"maxPriorityFeePerGas"`
+	MaxFeePerGas    string `json:"maxFeePerGas"`
 	Gas             string `json:"gas"`
 	Value           string `json:"value,omitempty"`
 	Data            string `json:"data"`
@@ -63,17 +69,42 @@ func bigIntToString(n int64) string {
 	return hex.EncodeToString(b)
 }
 
-func (anvil *AnvilClient) SendTransaction(from common.Address, tx *types.Transaction) common.Hash {
-	body := utils.GetJsonRPCRequestBody("eth_sendTransaction", anvilTransaction{
-		From:            from.Hex(),
-		To:              tx.To().Hex(),
-		GasPrice:        bigIntToString(tx.GasPrice().Int64()),
-		Gas:             bigIntToString(int64(tx.Gas())),
-		Data:            hex.EncodeToString(tx.Data()),
-		Nonce:           bigIntToString(int64(tx.Nonce())),
-		Value:           fmt.Sprintf("%x", tx.Value()),
-		TransactionType: bigIntToString(int64(tx.Type())),
-	})
+func (anvil *AnvilClient) SendTransaction(from common.Address, tx *types.Transaction, nonceArr ...int64) common.Hash {
+	var anvilTx anvilTransaction
+	nonce := func() (nonce int64) {
+		if nonceArr == nil {
+			nonce = int64(tx.Nonce())
+		} else {
+			nonce = nonceArr[0]
+		}
+		return
+	}()
+
+	if tx.Type() == 1 {
+		anvilTx = anvilTransaction{
+			From:            from.Hex(),
+			To:              tx.To().Hex(),
+			GasPrice:        hex.EncodeToString(tx.GasTipCap().Bytes()),
+			Gas:             bigIntToString(int64(tx.Gas())),
+			Data:            hex.EncodeToString(tx.Data()),
+			Nonce:           bigIntToString(nonce),
+			Value:           fmt.Sprintf("%x", tx.Value()),
+			TransactionType: bigIntToString(int64(tx.Type())),
+		}
+	} else {
+		anvilTx = anvilTransaction{
+			From:            from.Hex(),
+			To:              tx.To().Hex(),
+			MaxPriority:     hex.EncodeToString(tx.GasTipCap().Bytes()),
+			MaxFeePerGas:    hex.EncodeToString(tx.GasFeeCap().Bytes()),
+			Gas:             bigIntToString(int64(tx.Gas())),
+			Data:            hex.EncodeToString(tx.Data()),
+			Nonce:           bigIntToString(nonce),
+			Value:           fmt.Sprintf("%x", tx.Value()),
+			TransactionType: bigIntToString(int64(tx.Type())),
+		}
+	}
+	body := utils.GetJsonRPCRequestBody("eth_sendTransaction", anvilTx)
 	result, err := utils.JsonRPCMakeRequest(anvil.url, body)
 	if err != nil {
 		log.Fatal("from", from, "err:", err)
@@ -83,6 +114,11 @@ func (anvil *AnvilClient) SendTransaction(from common.Address, tx *types.Transac
 
 func (anvil *AnvilClient) SetEthBalance(to common.Address, value *big.Int) {
 	body := utils.GetJsonRPCRequestBody("anvil_setBalance", to.Hex(), fmt.Sprintf("%x", value))
+	_, err := utils.JsonRPCMakeRequest(anvil.url, body)
+	log.CheckFatal(err)
+}
+func (anvil *AnvilClient) EvmMine() {
+	body := utils.GetJsonRPCRequestBody("evm_mine")
 	_, err := utils.JsonRPCMakeRequest(anvil.url, body)
 	log.CheckFatal(err)
 }
