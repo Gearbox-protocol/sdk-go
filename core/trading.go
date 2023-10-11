80850222bcb02674fc11eb967222beef9fee60ff
5lliot <naijhsrah@gmail.com>
2023-10-12 02:20:15 +0700

    feat: add trading details

---
+++ core/trading.go
@@ -0,0 +1,25 @@
+package core
+
+var TRADING_SYMBOLS = []string{
+	"MKR",
+	"UNI",
+	"LINK",
+	"LDO",
+	"RPL",
+	"CRV",
+	"APE",
+	"CVX",
+	"FXS",
+	"BLUR",
+	"WETH",
+	"WBTC",
+}
+
+const OTHER_TRADING_BASE_SYMBOL = "DAI"
+
+func TradingSymNoW[T ~string](sym T) T {
+	if sym[0] == 'W' {
+		sym = sym[1:]
+	}
+	return sym
+}
