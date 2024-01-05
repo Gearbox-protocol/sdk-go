922c31d1049525b3f7da1ac57cc0dc3aecd34129
harsh-98 <harshjniitr@gmail.com>
2024-01-05 23:12:53 +0400

    feat: add correct entry/closePrice logic

--- calc/trading_base_token.go
+++ calc/trading_base_token.go
@@ -4,44 +4,9 @@ import (
 	"math/big"
 
 	"github.com/Gearbox-protocol/sdk-go/core"
-	"github.com/Gearbox-protocol/sdk-go/core/schemas"
-	"github.com/Gearbox-protocol/sdk-go/log"
 	"github.com/Gearbox-protocol/sdk-go/utils"
 	"github.com/ethereum/go-ethereum/common"
 )
-
-type PriceDS struct {
-	ClosePrice   float64 `gorm:"column:close_price" json:"closePrice"`
-	TradingToken string  `json:"tradingToken,omitempty"`
-	BaseToken    string  `json:"quoteToken,omitempty"`
-}
-
-type EntryPriceI interface {
-	GetBalances() core.DBBalanceFormat
-	GetBorrowedAmount() *big.Int
-	GetCollateralInUnderlying() interface{}
-	GetUnderlyingToken() string
-	SetCurrentPrice(tradingToken, quoteToken string, amt float64)
-}
-
-type tokenI interface {
-	GetToken(token string) *schemas.Token
-}
-
-func toBigInt(collateral interface{}, decimals int8) *big.Int {
-	switch v := collateral.(type) {
-	case float64:
-		ans, _ := new(big.Float).Mul(big.NewFloat(v), utils.GetExpFloat(decimals)).Int(nil)
-		return ans
-	case *big.Int:
-		return v
-	case *core.BigInt:
-		return v.Convert()
-	default:
-		log.Fatal("can't covert this type", v)
-		return nil
-	}
-}
 
 var _addrToSym map[common.Address]core.Symbol
 
@@ -53,7 +18,7 @@ func loadSymToAddrStore(chainId int64) map[common.Address]core.Symbol {
 }
 
 func TradingAndBaseTokens(chainId int64, bal core.DBBalanceFormat, underlying string) (tradingToken, baseToken string) {
-	otherToken, _, ok := singleEntryPriceToken(bal, underlying)
+	otherToken, _, ok := otherTokenAndItsBalance(bal, underlying)
 	if !ok {
 		return "", ""
 	}
@@ -86,7 +51,7 @@ func priority(m map[common.Address]core.Symbol, addr string) int {
 }
 
 // returns false if there are more than 1 token
-func singleEntryPriceToken(bal core.DBBalanceFormat, underlyingToken string) (ansToken string, ansamt *big.Int, ansOk bool) {
+func otherTokenAndItsBalance(bal core.DBBalanceFormat, underlyingToken string) (ansToken string, ansamt *big.Int, ansOk bool) {
 	tokens := 0
 	for token, details := range bal {
 		if details.IsEnabled && details.HasBalanceMoreThanOne() && // ignore tokens which are disabled or have zero balances
