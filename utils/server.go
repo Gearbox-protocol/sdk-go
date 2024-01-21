cb6613661b90a970d7e85e1d5195db93ebfa5a18
harsh-98 <harshjniitr@gmail.com>
2024-01-22 00:46:19 +0400

    feat: add getHost

--- utils/server.go
+++ utils/server.go
@@ -8,15 +8,23 @@ import (
 	"github.com/Gearbox-protocol/sdk-go/log"
 )
 
+func GetHost(host string) string {
+	if host == "0" || host == "" {
+		return ""
+	}
+	if _, err := strconv.ParseInt(host, 10, 64); err == nil {
+		host = fmt.Sprintf(":%s", host)
+	}
+	return host
+}
+
 // host = 0.0.0.0:8080
 // host = 8080
 // host =0 return
 func ServerFromMux(mux *http.ServeMux, host string) {
-	if host == "0" || host == "" {
+	host = GetHost(host)
+	if host == "" {
 		return
-	}
-	if _, err := strconv.ParseInt(host, 10, 64); err == nil {
-		host = fmt.Sprintf(":%s", host)
 	}
 	srv := http.Server{
 		Addr:    host,
