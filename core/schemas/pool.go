f6388a2f8935e0bd98eacd74e589869ed3ab8888
harsh-98 <harshjniitr@gmail.com>
2024-11-12 15:08:45 +0700

    fix: add market to tvl snapshot ds

--- core/schemas/pool.go
+++ core/schemas/pool.go
@@ -14,8 +14,8 @@ type PoolState struct {
 	BaseBorrowAPYBI        *core.BigInt     `gorm:"column:base_borrow_apy_bi"`
 	DepositAPYBI           *core.BigInt     `gorm:"column:deposit_apy_bi"`
 	Version                core.VersionType `gorm:"column:_version"`
-	Market string `gorm:"column:market" json:"market"`
-	PriceOracle PriceOracleT `gorm:"column:price_oracle" json:"priceOracle"`
+	Market                 string           `gorm:"column:market" json:"market"`
+	PriceOracle            PriceOracleT     `gorm:"column:price_oracle" json:"priceOracle"`
 	//
 	Name string `gorm:"column:name"`
 }
@@ -70,6 +70,7 @@ type TvlSnapshots struct {
 	AvailableLiquidity float64 `gorm:"column:available_liquidity"`
 	CATotalValue       float64 `gorm:"column:ca_total_value"`
 	ExpectedLiq        float64 `gorm:"column:expected_liq"`
+	Market             string  `gorm:"column:market;primaryKey"`
 }
 
 func (TvlSnapshots) TableName() string {
