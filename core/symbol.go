fb0cd12f3a805165308e27d9607c21232ea3a5df
harsh-98 <harshjniitr@gmail.com>
2025-06-13 07:37:29 +0530

    bnb: usdt

--- core/symbol.go
+++ core/symbol.go
@@ -214,6 +214,7 @@ func GetToken(chainId int64, token Symbol) common.Address {
 			"WBNB": "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
 			"BNB":  "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
 			"USDC": "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d",
+			"USDT": "0x55d398326f99059fF775485246999027B3197955",
 		},
 	}
 	network := log.GetBaseNet(chainId)
