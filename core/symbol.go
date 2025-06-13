604a24e67fcf16f4f3faca4d3ca8f523b55baa5f
harsh-98 <harshjniitr@gmail.com>
2025-06-13 07:43:17 +0530

    bnb: usdt

--- core/symbol.go
+++ core/symbol.go
@@ -209,12 +209,13 @@ func GetToken(chainId int64, token Symbol) common.Address {
 			"AAVE_V3_LENDING_POOL": "0x794a61358D6845594F94dc1DB02A252b5b4814aD",
 		},
 		log.BNB: {
-			"WBTC": "0x0555E30da8f98308EdB960aa94C0Db47230d2B9c",
-			"WETH": "0x2170Ed0880ac9A755fd29B2688956BD959F933F8",
-			"WBNB": "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
-			"BNB":  "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
-			"USDC": "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d",
-			"USDT": "0x55d398326f99059fF775485246999027B3197955",
+			"WBTC":                 "0x0555E30da8f98308EdB960aa94C0Db47230d2B9c",
+			"WETH":                 "0x2170Ed0880ac9A755fd29B2688956BD959F933F8",
+			"WBNB":                 "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
+			"BNB":                  "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+			"USDC":                 "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d",
+			"USDT":                 "0x55d398326f99059fF775485246999027B3197955",
+			"AAVE_V3_LENDING_POOL": "0x6807dc923806fE8Fd134338EABCA509979a7e0cB",
 		},
 	}
 	network := log.GetBaseNet(chainId)
