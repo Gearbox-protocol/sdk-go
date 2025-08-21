72b7fe82264833d41081640edd349f062458d252
harsh-98 <harshjniitr@gmail.com>
2025-08-21 16:36:04 +0400

    fix: oracle for ethereum aegis and other new external oracles

--- core/price_feed_type_v3.go
+++ core/price_feed_type_v3.go
@@ -113,14 +113,16 @@ func GetGearboxPfType(client ClientI, oracle string, token string) (int64, error
 				description = strings.ToLower(string(description))
 				if strings.Contains(description, "redstone") || // the oracles that don't have priceFeedType method,
 					GetBaseChainId(client) == 43111 ||
+					strings.Contains(strings.ToLower(description), "aegis oracle") || // like "Aegis Oracle - chainlinkOracle", // 0x600f888a50c66bC39Ac83523EBdbba20Da984173 on ethereum.
+					oracle == "0x2da05F177485264D432878D4A17d722bc64Db0EF" || // 0x2da05F177485264D432878D4A17d722bc64Db0EF on ethereum.
 					strings.Contains(description, "hemiBTC/USD") { // the oracles that don't have priceFeedType method,
 					if GetBaseChainId(client) == 43111 {
 						log.Warn("The oracle on HemiBTC. has this description and is an external oracle.", description)
 					}
+					log.Info(oracle == "0x2da05F177485264D432878D4A17d722bc64Db0EF")
 					// // are outside redstne oracle and in control of redstone team to update regularly so can be treated as curve pf
 					return V3_EXTERNAL, nil
 				}
-				log.Info()
 				log.Fatal(oracle, token, "priceFeedType failed: ", description, err)
 			}
 		}
